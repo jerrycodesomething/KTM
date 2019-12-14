d98ac4a64f61817ef768b9373bce2d1d8cf1c15a
ariqbinrazali <45396297+ariqbinrazali@users.noreply.github.com>
2019-12-14 15:12:45 +0800

    test1

--- src/ktm/UserNode.java
+++ src/ktm/UserNode.java
@@ -26,6 +26,54 @@ public class UserNode {
         this.ticketid=ticketid;
         next=null;
     } 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIcnum() {
+        return icnum;
+    }
+
+    public void setIcnum(int icnum) {
+        this.icnum = icnum;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getTicketid() {
+        return ticketid;
+    }
+
+    public void setTicketid(int ticketid) {
+        this.ticketid = ticketid;
+    }
+
+    public UserNode getNext() {
+        return next;
+    }
+
+    public void setNext(UserNode next) {
+        this.next = next;
+    }
     
     
     
