84234331404c39b3365c1193a3fd59d8055b9e32
Jeremiah <Jeremiah@DESKTOP-P7GG928>
2019-12-15 00:14:45 +0800

    Commit for booking

--- src/ktm/Booking.java
+++ src/ktm/Booking.java
@@ -6,14 +6,48 @@ import java.util.Scanner;
 
 public class Booking {
     //Booking Class Variables
-    
+    static String ui_state; 
     //Booking Constructor
     
     //Booking Methods
     public static void bookMain(){
-        System.out.println("------------- Travel Dates (2020)--------------");
-        System.out.println("Please select options 1-7...");
+        System.out.println("------------- Available Travel Dates (2020)--------------");
+        System.out.println("------------- Kuala Lumpur to Johor Bahru ----------------");
+        System.out.println("\nPlease select options 1-7...");
         System.out.println("\n1- Wednesday Jan 1 \n2- Thursday Jan 2 \n3- Friday Jan 3 \n4- Saturday Jan 4 \n5- Sunday Jan 5 \n6- Monday Jan 6 \n7- Tuesday Jan 7");
+        bookingMenu();
+    }
+    
+    public static void bookingMenu(){
+        Scanner s= new Scanner(System.in);
+        ui_state=s.nextLine();
+        
+        switch(ui_state){
+            case "1":
+                System.out.println("1");
+                break;
+            case "2":
+                System.out.println("2");
+                break;
+            case "3":
+                System.out.println("3");
+                break;
+            case "4":
+                System.out.println("4");
+                break;
+            case "5":
+                System.out.println("5");
+                break;
+            case "6":
+                System.out.println("6");
+                break;
+            case "7":
+                System.out.println("7");
+                break;  
+            default:
+                System.out.println("Please select options 1-7 only!");
+                bookingMenu();
+        }
     }
     
     public static void enterDetails(){
@@ -29,7 +63,7 @@ public class Booking {
         phone=s.nextLine();
         
         User user= new User();
-        user.addFirst(name, ic, phone, 0);
+
     }
     
     
